7cec1ff8b89cdb80e9a4ac1bd0ebfca0e9da01ac
Shuwei Hao <shuwei.hsw@alibaba-inc.com>
2019-05-29 19:52:07 +0800

    add custom DNS support and avoid overwriting

---
+++ scripts/updateDNS.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# unlock DNS file in case it was locked
+# chattr -i /etc/resolv.conf
+
+# Using your custom nameserver to replace xxx.xxx.xxx.xxx
+# echo "nameserver xxx.xxx.xxx.xxx\nnameserver xxx.xxx.xxx.xxx" > /etc/resolv.conf
+
+# Keep resolv locked to prevent overwriting by cloudinit/NetworkManager
+# chattr +i /etc/resolv.conf
