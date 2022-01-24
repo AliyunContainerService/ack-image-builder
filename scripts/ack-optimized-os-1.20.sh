bce6c33638e1a8796e039df7bc47daad51429402
skkkk <353263703@qq.com>
2022-01-24 10:47:19 +0800

    修改gpu拉包地址

--- scripts/ack-optimized-os-1.20.sh
+++ scripts/ack-optimized-os-1.20.sh
@@ -173,8 +173,9 @@ install_pkg() {
 }
 
 preset_gpu() {
+    GPU_PACKAGE_URL=http://aliacs-k8s-${REGION}.oss-${REGION}-internal.aliyuncs.com/public/pkg
     if [[ $PRESET_GPU ]]; then
-        bash -x pkg/run/$KUBE_VERSION/bin/nvidia-gpu-installer.sh
+        bash -x pkg/run/$KUBE_VERSION/bin/nvidia-gpu-installer.sh --package-url-prefix=${GPU_PACKAGE_URL}
     fi
 }
 
