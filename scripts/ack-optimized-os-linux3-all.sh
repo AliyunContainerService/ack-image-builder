da6a797bfee4103e228e1c3ad6aad89c889a357c
“skkkkkkk” <sk01199367@alibaba-inc.com>
2023-02-16 10:49:25 +0800

    adapter aliyun linux3

---
+++ scripts/ack-optimized-os-linux3-all.sh
@@ -0,0 +1,242 @@
+#!/bin/bash
+
+set -x
+set -e
+
+usage() {
+    cat >&2 <<-EOF
+Usage:
+    $0 -r RUNTIME [-s]
+
+Flags:
+    -r: sepcify container runtime, available value: docker and containerd
+    -s: skip security upgrade
+
+Example:
+    $0 -r docker -s
+    $0 -r docker
+    $0 -r containerd -s
+    $0 -r containerd
+EOF
+    exit 1
+}
+
+check_params() {
+    while getopts "r:sh" opt; do
+        case $opt in
+        r) RUNTIME="$OPTARG" ; ;;
+        s) SKIP_SECURITY_FIX="1" ; ;;
+        h | ?) usage ; ;;
+        esac
+    done
+
+    if [[ -z $RUNTIME ]] || [[ $RUNTIME != "docker" && $RUNTIME != "containerd" ]]; then
+        echo "ERROR: RUNTIME must not be empty, only support 'docker' and 'containerd' "
+        usage
+    fi
+}
+
+setup_env() {
+    export RUNTIME
+    export OS="AliyunOS"
+
+    if [[ "$RUNTIME" = "docker" ]]; then
+      export RUNTIME_VERSION="19.03.5"
+      export DOCKER_VERSION="19.03.5"
+    else
+      export RUNTIME_VERSION="1.5.10"
+    fi
+
+    export REGION=$(curl --retry 10 -sSL http://100.100.100.200/latest/meta-data/region-id)
+    export PKG_FILE_SERVER="http://aliacs-k8s-$REGION.oss-$REGION-internal.aliyuncs.com/$BETA_VERSION"
+    export ACK_OPTIMIZED_OS_BUILD=1
+}
+
+
+download_pkg() {
+    curl --retry 4 $PKG_FILE_SERVER/public/pkg/run/run-${KUBE_VERSION}.tar.gz -O
+    tar -xvf run-${KUBE_VERSION}.tar.gz
+}
+
+
+source_file() {
+    if [[ -e "pkg/run/$KUBE_VERSION/kubernetes.sh" ]]; then
+      source pkg/run/$KUBE_VERSION/kubernetes.sh --role source
+      install_pkg
+    elif [[ -e "pkg/run/$KUBE_VERSION/bin/kubernetes.sh" ]]; then
+      ROLE=deploy-nodes pkg/run/$KUBE_VERSION/bin/kubernetes.sh
+    fi
+}
+
+install_pkg() {
+    public::common::sync_ntpd
+    public::common::install_package
+}
+
+preset_gpu() {
+    GPU_PACKAGE_URL=http://aliacs-k8s-${REGION}.oss-${REGION}-internal.aliyuncs.com/public/pkg
+    if [[ "$PRESET_GPU" == "true" ]]; then
+        bash -x pkg/run/$KUBE_VERSION/bin/nvidia-gpu-installer.sh --package-url-prefix ${GPU_PACKAGE_URL}
+    fi
+}
+
+trim_os() {
+    local pkg_list="acl
+aic94xx-firmware
+aliyun-cli
+alsa-firmware
+alsa-lib
+alsa-tools-firmware
+authconfig
+avahi-libs
+bind-libs-lite
+bind-license
+biosdevname
+btrfs-progs
+cloud
+device-mapper-event
+device-mapper-event-libs
+dmraid
+dmraid-events
+dosfstools
+ed
+file
+firewalld
+firewalld-filesystem
+freetype
+fxload
+GeoIP
+geoipupdate
+gettext
+gettext-libs
+glibc-devel
+groff-base
+hunspell
+hunspell-en
+hunspell-en-GB
+hunspell-en-US
+ivtv-firmware
+iwl1000-firmware
+iwl100-firmware
+iwl105-firmware
+iwl135-firmware
+iwl2000-firmware
+iwl2030-firmware
+iwl3160-firmware
+iwl3945-firmware
+iwl4965-firmware
+iwl5000-firmware
+iwl5150-firmware
+iwl6000-firmware
+iwl6000g2a-firmware
+iwl6000g2b-firmware
+iwl6050-firmware
+iwl7260-firmware
+jansson
+kbd
+kbd-legacy
+kbd-misc
+libaio
+libdrm
+libmpc
+libpciaccess
+libpng
+lm_sensors-libs
+lsscsi
+lvm2
+m4
+mailx
+man-db
+mariadb-libs
+mdadm
+microcode_ctl
+patch
+perl-Getopt-Long
+plymouth
+plymouth-scripts
+postfix
+python3
+python3-pip
+python3-setuptools
+python-decorator
+python-IPy
+rng-tools
+rsync
+sgpio
+slang
+spax
+strace
+sysstat
+tcpdump
+teamd
+vim-common
+vim-enhanced
+vim-filesystem
+wl1000-firmware
+wpa_supplicant
+xfsprogs
+"
+    yum remove -y $pkg_list
+    rm -rf /lib/modules/$(uname -r)/kernel/drivers/{media,staging,gpu,usb}
+    rm -rf /boot/*-rescue-* /boot/*3.10.0* /usr/share/{doc,man} /usr/src
+}
+
+pull_image() {
+    if [[ "$RUNTIME" = "docker" ]]; then
+        systemctl start docker
+        sleep 10
+
+        docker pull registry-vpc.${REGION}.aliyuncs.com/acs/kube-proxy:v${KUBE_VERSION}
+        docker pull registry-vpc.${REGION}.aliyuncs.com/acs/pause:3.2
+        docker pull registry-vpc.${REGION}.aliyuncs.com/acs/coredns:1.6.7
+    else
+        systemctl start containerd
+        sleep 10
+
+        ctr -n k8s.io i pull registry-vpc.${REGION}.aliyuncs.com/acs/kube-proxy:v${KUBE_VERSION}
+        ctr -n k8s.io i pull registry-vpc.${REGION}.aliyuncs.com/acs/pause:3.2
+        ctr -n k8s.io i pull registry-vpc.${REGION}.aliyuncs.com/acs/coredns:1.6.7
+    fi
+}
+
+update_os_release() {
+    if [[ ! -f /etc/image-id ]]; then
+      touch /etc/image-id
+    fi
+    sed -i  "s#LTS#LTS ACK-Optimized-OS#"  /etc/image-id
+}
+
+record_k8s_version() {
+    cat > /etc/ACK-Optimized-OS <<-EOF
+kubelet=$KUBE_VERSION
+docker=$DOCKER_VERSION
+EOF
+}
+
+post_install() {
+    if [[ $SKIP_SECURITY_FIX ]]; then
+        touch /var/.skip-security-fix
+    fi
+}
+
+cleanup() {
+    rm -rf ./{addon*,docker*,kubernetes*,pkg,run*}
+}
+
+main() {
+    trap 'cleanup' EXIT
+
+    check_params "$@"
+    setup_env
+
+#    trim_os
+
+    download_pkg
+    source_file
+    preset_gpu
+    pull_image
+    update_os_release
+    record_k8s_version
+}
+
+main "$@"
